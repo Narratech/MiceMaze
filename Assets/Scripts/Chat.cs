71fd499d7cd3f65303cd807f2e411339c3585bcd
viklazaro <viklazaro@gmail.com>
2018-04-09 19:08:34 +0200

    Chat Online
    Añadido un chat online. Por el momento se puede usar en cualquier momento. Pero está pensado para la parte del interrogatorio.

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Chat : NetworkBehaviour {
+
+	Text texto;
+	InputField input;
+
+	// Use this for initialization
+	void Start () {
+		texto = GameObject.Find ("PanelTexto").GetComponent<Text> ();
+		input = GameObject.Find ("EntradaTexto").GetComponent<InputField> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			if (input.text != "") {
+				string mensaje = input.text;
+				input.text = "";
+
+				CmdEnviar (mensaje);
+			}
+		}
+	}
+
+	[Command]
+	void CmdEnviar(string mensaje){
+		
+		RpcRecivir (mensaje);
+	}
+
+	[ClientRpc]
+	public void RpcRecivir(string mensaje){
+
+		texto.text += ">>" + mensaje + "\n";
+	}
+
+}
